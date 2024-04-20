374914859255a890def9156f03618894b25e2a37
asankanung <austincasa21@gmail.com>
2024-04-20 12:45:47 -0400

    billing created
    Billing page beginning

---
+++ frontend/src/pages/BillingPage.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+hello world!
+
+    </div>
+</template>
+  <script>
+  import { defineComponent } from "vue";
+  
+  export default defineComponent({
+    name: "test-test",
+    setup() {
+      return {};
+    },
+    data() {
+      return {};
+    },
+    mounted() {},
+    components: {},
+  });
+  </script>
+  
