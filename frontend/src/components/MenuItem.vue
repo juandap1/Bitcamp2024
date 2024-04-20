64720d773bd3a20c67d3b6f99beac9272ea7da31
jwang517 <142756571+jwang517@users.noreply.github.com>
2024-04-20 16:24:50 -0400

    aoijroaij

---
+++ frontend/src/components/MenuItem.vue
@@ -0,0 +1,79 @@
+<template>
+    <div class="food-item">
+          <img :src="img">
+          <div class="price-label">{{ price }}</div>
+          <br>
+          <div class="name-label">{{ name }}</div>
+          <button class="MinusButton" @click="decrementCount">-</button>
+          <label class="items" id="mozzarella-count">{{ count }}</label>
+          <button class="PlusButton" @click="incrementCount">+</button> 
+    </div>
+  </template>
+  <script>
+  import { defineComponent } from "vue";
+  
+  export default defineComponent({
+    name: "menu-item",
+    props :{
+        name: {
+            type: String,
+            required: true
+        },
+        img: {
+            type: String,
+            required: true
+        },
+        price: {
+            type: String,
+            required: true
+        }
+    },
+    setup() {
+      return {};
+    },
+    data() {
+      return {
+        count : 0
+      };
+    },
+    methods: {
+    incrementCount () {
+      this.count++;
+      list-of-items.append(this.name)
+    },
+    decrementCount (){
+      if(this.count > 0){
+        this.count --;
+        list-of-items.remove(this.name)
+      }
+    }
+  },
+    mounted() {},
+    components: {},
+  });
+  </script>
+<style scoped>
+img{
+  border-radius: 50%;
+  width: 200px;
+  height: 200px;
+  margin-left: 25px;
+}
+.name-label{
+    word-wrap: break-word;
+    margin-left: 45px;
+    width: 150px;
+}
+.price-label{
+    text-align: center;
+    position:absolute;
+    background-color: var(--tertiary);
+    border-radius: 50%;
+    padding: 5px;
+}
+.food-item{
+word-wrap: break-word;
+  text-align: center;
+  font-family: Georgia;
+}
+</style>
--- frontend/src/pages/MenuPage.vue
+++ frontend/src/pages/MenuPage.vue
@@ -4,20 +4,30 @@
 
     <h2>Appetizers</h2>
     <hr>
-    <form>
-      <img src="https://media.olivegarden.com/en_us/images/product/Fried-Mozzarella-gv-590X365.jpg">
-      <input type="button" class="MinusButton">
-      <input type="button" class="PlusButton">
-    </form>
+    <div class="item-container">
+      <menu-item v-for="m in items" v-bind="m" :key="m" />
+    </div>
+      
 
     <h2>Entrees</h2>
     <hr>
+    <div class="item-container">
+      <menu-item v-for="m in Entrees" v-bind="m" :key="m" />
+    </div>
 
     <h2>Desserts</h2>
     <hr>
+    <div class="item-container">
+      <menu-item v-for="m in Desserts" v-bind="m" :key="m" />
+    </div>
 
     <h2>Drinks</h2>
     <hr>
+    <div class="item-container">
+      <menu-item v-for="m in Drinks" v-bind="m" :key="m" />
+    </div>
+    <hr>
+    <button class="submit" onclick="checkout">Check Out</button>
 
   </div>
 
@@ -26,27 +36,116 @@
 <script>
 import { ref } from "vue";
 import { defineComponent } from "vue";
+import MenuItem from "../components/MenuItem.vue"
 
 export default defineComponent({
   name: "test-test",
-  props :{
-
-  },
+  
   setup() {
-    // const url = ref('')
     return {
-      // url, 
-      // refresh(){
-      //   url.value = " " + Math.random()
-      // }
     };
   },
   data() {
     return {
+      list-of-items: [],
+      items: [
+        {
+          name: "Fried Mozzarella",
+          img: "https://media.olivegarden.com/en_us/images/product/Fried-Mozzarella-gv-590X365.jpg",
+          price: "$8.29"
+        },
+        {
+          name:"Calamari",
+          img:"https://media.olivegarden.com/en_us/images/product/calamari-dpv-590x365.jpg",
+          price: "$11.79"
+        },
+        {
+          name:"Lasagna Fritta",
+          img:"https://media.olivegarden.com/en_us/images/product/Lasagna-Fritta-gv-590x365.jpg",
+          price:"$10.99"
+        },
+        {
+          name:"Breadsticks with Dipping sauces",
+          img:"https://media.olivegarden.com/en_us/images/product/Dipping-Sauce-gv-590x365.jpg",
+          price:"$4.49"
+        }
+      ],
+      Entrees: [
+        {
+          name:"Chicken Alfredo",
+          img:"https://media.olivegarden.com/en_us/images/product/classic-chicken-alfredo-dinner-dpv-590x365.jpg",
+          price:"$19.49"
+        },
+        {
+          name:"Chicken Parmigiana",
+          img:"https://media.olivegarden.com/en_us/images/product/dinner-chicken-parm-dpv-590x365.jpg",
+          price:"$19.49"
+        },
+        {
+          name:"Chicken Tortelloni Alfredo",
+          img:"https://media.olivegarden.com/en_us/images/product/asiago-tortelloni-alfredo-with-grilled-chicken-gv-590x365.png",
+          price:"$21.49"
+        },
+        {
+          name:"Herb-Grilled Salmon",
+          img:"https://media.olivegarden.com/en_us/images/product/HerbGrilledSalmon_v1_gv.jpg",
+          price:"$20.99"
+        }
+      ],
+      Desserts:[
+        {
+          name:"Chocolate Lasagna",
+          img:"https://media.olivegarden.com/en_us/images/marketing/Chocolate_Lasagna_590X365.jpg",
+          price:"$9.29"
+        },
+        {
+          name:"Tiramisu",
+          img:"https://media.olivegarden.com/en_us/images/marketing/Tiramisu-gv-590x365.jpg",
+          price:"$8.99"
+        },
+        {
+          name:"Strawberry Cream Cake (V)",
+          img:"https://media.olivegarden.com/en_us/images/marketing/Strawberry-Cream-Cake-Updated-gv-590x365.jpg",
+          price:"$9.29"
+        },
+        {
+          name:"Sicilian Cheesecake With Strawberry Topping (V)",
+          img: "https://media.olivegarden.com/en_us/images/marketing/Strawberry-Cheesecake-gv-590x365.jpg",
+          price: "$8.99"
+        }
+      ],
+      Drinks: [
+        {
+          name:"2-liter Soda",
+          img:"https://media.olivegarden.com/en_us/images/product/d-two-liter-sodas-dpv.jpg",
+          price:"$4.49"
+        },
+        {
+          name:"Raspberry Lemonade",
+          img:"https://media.olivegarden.com/en_us/images/product/d-raspberry-lemonade-dpv.JPG",
+          price:"$4.29"
+        },
+        {
+          name:"Cappucino",
+          img:"https://media.olivegarden.com/en_us/images/product/d-cappuccino-with-whipped-cream-dpv.jpg",
+          price:"$4.99"
+        },
+        {
+          name:"Sweet Pink Moscato Blend Confetti",
+          img:"https://media.olivegarden.com/en_us/images/product/confetti-sweet-moscato-blend-wine-dpv-590x365.jpg",
+          price:"$24.00"
+        }
+      ]
     };
   },
+  methods: {
+    checkout(){
+
+    }
+  }
+
   mounted() {},
-  components: {},
+  components: {MenuItem},
 });
 </script>
 <style scoped>
@@ -60,6 +159,10 @@ h2{
   font-style: italic;
   text-align: left;
   text-indent: 30px;
+}
+.item-container{
+  justify-content: space-around;
+  display: flex;
 }
 hr{
   color: var(--tertiary)
