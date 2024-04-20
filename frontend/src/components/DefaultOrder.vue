1371922c6483c939ce256575bf8d39605935b244
asankanung <austincasa21@gmail.com>
2024-04-20 16:57:38 -0400

    billing page update

---
+++ frontend/src/components/DefaultOrder.vue
@@ -0,0 +1,27 @@
+<template>
+    <q-item>
+        <q-item-section>
+        <q-item-label>{{ name }}</q-item-label>
+        <q-item-label caption>${{ price }}</q-item-label>
+        </q-item-section>
+    </q-item>
+</template>
+  <script>
+  import { defineComponent } from "vue";
+  
+  export default defineComponent({
+    name: "test-test",
+    props: {
+        name: String,
+        price: Number
+    },
+    setup() {
+      return {};
+    },
+    data() {
+      return {};
+    },
+    mounted() {},
+    components: {},
+  });
+  </script>
--- frontend/src/pages/BillingPage.vue
+++ frontend/src/pages/BillingPage.vue
@@ -1,22 +1,114 @@
 <template>
-    <div>
-        THIS IS BILLING
+    <div class="main">
+        <div class="heading">
+            Billing
+        </div>
+        <div class="sub-heading">
+            Payment Information
+        </div>
 
+        <div class="sub-heading">
+            Bill:
+        </div>
+
+        <div class="q-pa-md" style="max-width: 350px">
+            <q-list bordered separator>
+                <DefaultOrder v-for="i in items" v-bind="i" :key="i"></DefaultOrder>
+            </q-list>
+        </div>
+        <p>Subtotal: ${{ total }}</p>
+        <p>Total: ${{ total*1.06 }}</p>
+
+        <div class="q-pa-md q-gutter-sm">
+            <q-btn :ripple="{ center: true }" outline push label="Pay Check" no-caps @click="payFull"/>
+            <q-btn :ripple="{ center: true }" outline push label="Split Check" no-caps @click="splitCheck"/>
+        </div>
+
+        
+
+        
     </div>
 </template>
   <script>
   import { defineComponent } from "vue";
+  import DefaultOrder from '../components/DefaultOrder.vue';
   
   export default defineComponent({
     name: "test-test",
     setup() {
-      return {};
+        return {};
     },
     data() {
-      return {};
+      return {
+        items: [
+            {
+                name: "Calamari",
+                price: 10
+            },
+            {
+                name: "Pizza",
+                price: 7
+            },
+            {
+                name: "Cheeseburger",
+                price: 12
+            },
+            {
+                name: "Mozzarella Sticks",
+                price: 8
+            }
+        ],
+      };
+    },
+    methods: {
+        payFull(){
+            console.log('YOU ARE PAYING THE WHOLE CHECK!!');
+            const total = this.total
+            const confirmed = confirm('Are you sure you are ready to pay?');
+            if(confirmed){
+                this.$router.push('/confirmation');
+            }
+        },
+
+        splitCheck(){
+            console.log('YOU ARE SPLITTING THE CHECK!!!');
+        },
+
+    },
+    computed:{
+        total(){
+            let total = 0;
+            this.items.forEach(element => {
+                total += element.price;
+            });
+            return total;
+        }
     },
     mounted() {},
-    components: {},
+    components: {DefaultOrder},
   });
+
   </script>
-  
+<style scoped>
+    .heading {
+        font-size: 70px;
+        color: var(--secondary);
+        background-color: var(--tertiary);
+        text-align: center;
+    }
+
+    .main{
+        background-color: var(--primary);
+        height: 100vh;
+    }
+
+    .sub-heading{
+        font-size: 50px;
+        color: darkslategray;
+    }
+
+    .q-btn{
+        color: var(--secondary);
+    }
+
+</style>
