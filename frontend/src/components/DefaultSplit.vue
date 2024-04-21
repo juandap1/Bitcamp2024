e5899261cf8d420e8d705c94c8471f1880573f02
asankanung <austincasa21@gmail.com>
2024-04-21 06:39:27 -0400

    selections

---
+++ frontend/src/components/DefaultSplit.vue
@@ -0,0 +1,49 @@
+<template>
+    <q-item>
+        <q-item-section>
+          <q-item-label>{{ name }}</q-item-label>
+          <q-item-label caption>${{ price }}</q-item-label>
+        </q-item-section>
+        <q-item-section side>
+          <div class="cnt-nmb">{{ count }}</div>
+        </q-item-section>
+        <q-select
+        required
+        v-model="single"
+        multiple
+        :options="people"
+        label="splits"
+        />
+    </q-item>
+</template>
+  <script>
+  import { defineComponent } from "vue";
+  
+  export default defineComponent({
+    name: "test-test",
+    props: {
+        name: String,
+        price: Number,
+        count: Number
+    },
+    setup() {
+      return {};
+    },
+    data() {
+      return {
+        people: ["You", "Guest1","Guest2"]
+      };
+    },
+    mounted() {},
+    components: {},
+  });
+  </script>
+  <style scoped>
+  .cnt-nmb {
+    padding: 2.5px;
+    border-radius: 2.5px;
+    background-color: #222;
+    color: white;
+    line-height: 1;
+  }
+  </style>
--- frontend/src/pages/SplitPage.vue
+++ frontend/src/pages/SplitPage.vue
@@ -1,17 +1,17 @@
 <template>
     <div class="main">
-    <div class="heading">Pay Now</div>
+    <div class="heading">Split Bill</div>
     <div class="main-inner">
       <div class="row">
         <div class="col-6">
           <div class="q-px-md">
             <div class="s-lbl">Order</div>
             <q-list bordered separator v-if="store.session != null">
-              <DefaultOrder
+              <DefaultSplit
                 v-for="i in store.session.items"
                 v-bind="i"
                 :key="i"
-              ></DefaultOrder>
+              ></DefaultSplit>
             </q-list>
           </div>
         </div>
@@ -64,12 +64,33 @@
             </div>
           </div>
         </div>
+
+
+        <q-btn 
+        class="full-width"
+        :ripple="{center: true}"
+        outline
+        push
+        no-caps
+        label ="Done"
+        @click="doneSplit">
+        </q-btn>
+        <q-popup-proxy class="bg-transparent">
+                  <div class="bg-dark q-py-md q-px-xl">
+                    <q-btn class="bg-white text-black" @click="payFull">
+                      Confirm Payment of ${{ total }}?
+                    </q-btn>
+                  </div>
+                </q-popup-proxy>
       </div>
     </div>
   </div>
 </template>
   <script>
-  import { defineComponent } from "vue";
+import { defineComponent } from "vue";
+import DefaultSplit from "../components/DefaultSplit.vue";
+import { useStateStore } from "src/stores/state";
+import PaymentMethodSelect from "../components/PaymentMethodSelect.vue";
   
   export default defineComponent({
     name: "test-test",
@@ -79,6 +100,25 @@
     data() {
       return {};
     },
+    methods:{
+        doneSplit(){
+
+        }
+    },
+    computed: {
+    subtotal() {
+      let total = 0;
+      if (useStateStore().session?.items != null) {
+        useStateStore().session.items.forEach((x) => {
+          total += x.price * x.count;
+        });
+      }
+      return total;
+    },
+    total() {
+      return Math.round(this.subtotal * 1.06 * 100) / 100;
+    },
+  },
     mounted() {},
     components: {},
   });
