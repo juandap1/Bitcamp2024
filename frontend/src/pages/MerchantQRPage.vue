c5cff7b3f8c54ac3e65ae3556b2b9655f550826b
Matthew Branning <48932625+juandap1@users.noreply.github.com>
2024-04-21 01:53:14 -0400

    qr code merchant page

---
+++ frontend/src/pages/MerchantQRPage.vue
@@ -0,0 +1,80 @@
+<template>
+  <div class="pg-cont">
+    <div class="qr-cont shadow-6">
+      <img
+        class="logo"
+        src="https://1000logos.net/wp-content/uploads/2023/09/Olive-Garden-Logo.jpg"
+      />
+      <div class="qr-wrap">
+        <q-img :src="qrCode" />
+      </div>
+      <q-btn class="q-mt-md" @click="$router.push(redir)">
+        Click Instead
+      </q-btn>
+    </div>
+  </div>
+</template>
+<script>
+import { defineComponent } from "vue";
+import QRCode from "qrcode";
+import { v4 as uuidv4 } from "uuid";
+
+export default defineComponent({
+  name: "MerchantQRPage",
+  setup() {
+    return {};
+  },
+  data() {
+    return {
+      qrCode: null,
+    };
+  },
+  methods: {
+    loadQr() {
+      QRCode.toDataURL(`${window.location.origin}/#/${this.redir}`)
+        .then((url) => {
+          this.qrCode = url;
+        })
+        .catch((err) => {
+          console.error(err);
+        });
+    },
+  },
+  mounted() {
+    this.loadQr();
+  },
+  components: {},
+  computed: {
+    redir() {
+      return `/menu/${this.$route.params.id}?session=${uuidv4()}`;
+    },
+  },
+});
+</script>
+<style scoped>
+.pg-cont {
+  width: 100vw;
+  height: calc(100vh - 50px);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.qr-cont {
+  padding: 20px;
+  background-color: white;
+  border-radius: 15px;
+  width: 400px;
+  text-align: center;
+}
+
+.logo {
+  height: 70px;
+}
+
+.qr-wrap {
+  border: 2px solid black;
+  padding: 5px;
+  border-radius: 7.5px;
+}
+</style>
--- frontend/src/router/routes.js
+++ frontend/src/router/routes.js
@@ -1,21 +1,28 @@
 const routes = [
   {
-    path: '/',
-    component: () => import('layouts/MainLayout.vue'),
+    path: "/",
+    component: () => import("layouts/MainLayout.vue"),
     children: [
-      { path: '', component: () => import('pages/IndexPage.vue') },
-      { path: 'billing', component: () => import('pages/BillingPage.vue') },
-      { path: 'confirmation', component: () => import('pages/ConfirmationPage.vue') },
-      { path: 'menu', component: () => import('pages/MenuPage.vue') }
-    ]
+      { path: "", component: () => import("pages/IndexPage.vue") },
+      { path: "billing/:id", component: () => import("pages/BillingPage.vue") },
+      {
+        path: "confirmation",
+        component: () => import("pages/ConfirmationPage.vue"),
+      },
+      { path: "menu/:id", component: () => import("pages/MenuPage.vue") },
+      {
+        path: "merchant/:id",
+        component: () => import("pages/MerchantQRPage.vue"),
+      },
+    ],
   },
 
   // Always leave this as last one,
   // but you can also remove it
   {
-    path: '/:catchAll(.*)*',
-    component: () => import('pages/ErrorNotFound.vue')
-  }
-]
+    path: "/:catchAll(.*)*",
+    component: () => import("pages/ErrorNotFound.vue"),
+  },
+];
 
-export default routes
+export default routes;
