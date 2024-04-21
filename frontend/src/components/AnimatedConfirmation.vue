9975edf17a6c3538ae5c149239571d9bf3c9ab62
Matthew Branning <48932625+juandap1@users.noreply.github.com>
2024-04-21 01:22:42 -0400

    confirmation page

---
+++ frontend/src/components/AnimatedConfirmation.vue
@@ -0,0 +1,122 @@
+<template>
+  <div>
+    <svg
+      width="133px"
+      height="133px"
+      viewBox="0 0 133 133"
+      version="1.1"
+      xmlns="http://www.w3.org/2000/svg"
+      xmlns:xlink="http://www.w3.org/1999/xlink"
+    >
+      <g
+        id="check-group"
+        stroke="none"
+        stroke-width="1"
+        fill="none"
+        fill-rule="evenodd"
+      >
+        <circle
+          id="filled-circle"
+          fill="#78B348"
+          cx="66.5"
+          cy="66.5"
+          r="54.5"
+        ></circle>
+        <circle
+          id="white-circle"
+          fill="#FFFFFF"
+          cx="66.5"
+          cy="66.5"
+          r="55.5"
+        ></circle>
+        <circle
+          id="outline"
+          stroke="#78B348"
+          stroke-width="4"
+          cx="66.5"
+          cy="66.5"
+          r="54.5"
+        ></circle>
+        <polyline
+          id="check"
+          stroke="#FFFFFF"
+          stroke-width="4"
+          points="41 70 56 85 92 49"
+        ></polyline>
+      </g>
+    </svg>
+  </div>
+</template>
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "AnimatedConfirmation",
+  setup() {
+    return {};
+  },
+  data() {
+    return {};
+  },
+  mounted() {},
+  components: {},
+});
+</script>
+<style scoped>
+@keyframes outline {
+  from {
+    stroke-dasharray: 0, 345.576px;
+  }
+  to {
+    stroke-dasharray: 345.576px, 345.576px;
+  }
+}
+#outline {
+  animation: 0.38s ease-in outline;
+  transform: rotate(0deg);
+  transform-origin: center;
+}
+
+@keyframes circle {
+  from {
+    transform: scale(1);
+  }
+  to {
+    transform: scale(0);
+  }
+}
+#white-circle {
+  animation: 0.35s ease-in 0.35s forwards circle;
+  transform: none;
+  transform-origin: center;
+}
+
+@keyframes check {
+  from {
+    stroke-dasharray: 0, 75px;
+  }
+  to {
+    stroke-dasharray: 75px, 75px;
+  }
+}
+#check {
+  animation: 0.34s cubic-bezier(0.65, 0, 1, 1) 0.8s forwards check;
+  stroke-dasharray: 0, 75px;
+}
+
+@keyframes check-group {
+  from {
+    transform: scale(1);
+  }
+  50% {
+    transform: scale(1.09);
+  }
+  to {
+    transform: scale(1);
+  }
+}
+#check-group {
+  animation: 0.32s ease-in-out 1.03s check-group;
+  transform-origin: center;
+}
+</style>
--- frontend/src/pages/ConfirmationPage.vue
+++ frontend/src/pages/ConfirmationPage.vue
@@ -1,21 +1,44 @@
 <template>
-    <div>
-        THIS IS CONFIRMATION
+  <div class="center-div">
+    <div class="inner">
+      <animated-confirmation />
+      <h6>Purchase Confirmed</h6>
+      <q-btn class="q-mt-md" @click="$router.push('/')">Return Home</q-btn>
     </div>
+  </div>
 </template>
-  <script>
-  import { defineComponent } from "vue";
-  
-  export default defineComponent({
-    name: "test-test",
-    setup() {
-      return {};
-    },
-    data() {
-      return {};
-    },
-    mounted() {},
-    components: {},
-  });
-  </script>
-  
+<script>
+import { defineComponent } from "vue";
+import AnimatedConfirmation from "../components/AnimatedConfirmation.vue";
+
+export default defineComponent({
+  name: "test-test",
+  setup() {
+    return {};
+  },
+  data() {
+    return {};
+  },
+  mounted() {},
+  components: { AnimatedConfirmation },
+});
+</script>
+<style scoped>
+.center-div {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: calc(100vh - 50px);
+}
+
+.inner {
+  text-align: center;
+}
+
+.inner h6 {
+  margin: 0;
+  margin-top: 15px;
+  font-size: 36px;
+  font-weight: bold;
+}
+</style>
